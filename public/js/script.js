878a899d0c8554f481f6a1cd36c6ee2455ff6a55
Aligollez <59908197+aligollez@users.noreply.github.com>
2024-02-23 12:42:55 +0300

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -12,7 +12,7 @@ $(function () {
   
   function submit() {
   var isim 			= $('#isim').val();
-  var regphone 		=  /^(\+90)?5[0-6][0-9]\d{8}$/;
+  var regphone 		=  /05[0,1,2,3,4,5,6,7,8,9][0-9]\d\d\d\d\d\d\d$/;
   var yas 			= $('#yas').val();
   var il 				= $('#il').val();
   var ilce 			= $('#ilce').val();
