c7ee867670ef8d5e8096c18f0901a075db88b6ec
Aligollez <59908197+aligollez@users.noreply.github.com>
2024-02-24 09:42:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-// server.js
-
 const express = require('express');
 const next = require('next');
-const axios = require('axios');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -11,35 +9,14 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
-  // POST isteğini işleyen endpoint
-  server.post('/analytics', async (req, res) => {
-    try {
-      // PHP sunucuya POST isteği gönderme
-      const phpResponse = await axios.post('https://jojobets935.com/tr/analytics.php');
-
-      // PHP sunucudan gelen veriyi kullanarak bir JSON yanıtı oluşturma
-      return res.json({ phpData: phpResponse.data });
-    } catch (error) {
-      console.error('PHP sunucu ile iletişim hatası:', error);
-      return res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
-
-  // Ziyaretçilerin her 2 saniyede bir POST isteği gönderdiği fonksiyon
-  const sendAnalyticsData = async () => {
-    try {
-      // Ziyaretçinin POST işlemini gerçekleştir
-      await axios.post('http://localhost:3000/analytics');
-
-      // 2 saniyede bir bu fonksiyonu çağır
-      setTimeout(sendAnalyticsData, 2000);
-    } catch (error) {
-      console.error('POST isteği hatası:', error);
-    }
-  };
-
-  // İlk çalıştırmayı yap
-  sendAnalyticsData();
+  // API isteklerini reverse proxy ile yönlendirme
+  server.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://jojobets935.com/tr/analytics.php', // Hedef URL
+      changeOrigin: true,
+    })
+  );
 
   server.all('*', (req, res) => {
     return handle(req, res);
