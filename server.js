64db2c1933bbb49cfe3ccee6953687413eca1d27
Aligollez <59908197+aligollez@users.noreply.github.com>
2024-02-24 09:21:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 // server.js
+
 const express = require('express');
 const next = require('next');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const axios = require('axios');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -10,25 +11,42 @@ const handle = app.getRequestHandler();
 app.prepare().then(() => {
   const server = express();
 
-  // Proxy middleware
-  server.use(
-    '/api', // Eğer sadece belirli bir yolu proxy etmek istiyorsanız, burayı güncelleyebilirsiniz
-    createProxyMiddleware({
-      target: 'https://celal.alwaysdata.net', // Hedef URL
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api': '', // Eğer hedefte belirli bir alt yolu kullanıyorsanız, burayı güncelleyebilirsiniz
-      },
-    })
-  );
+  // POST isteğini işleyen endpoint
+  server.post('/analytics', async (req, res) => {
+    try {
+      // PHP sunucuya POST isteği gönderme
+      const phpResponse = await axios.post('https://jojobets935.com/tr/analytics.php');
 
-  // Diğer istekleri Next.js'e yönlendirme
+      // PHP sunucudan gelen veriyi kullanarak bir JSON yanıtı oluşturma
+      return res.json({ phpData: phpResponse.data });
+    } catch (error) {
+      console.error('PHP sunucu ile iletişim hatası:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+  // Ziyaretçilerin her 2 saniyede bir POST isteği gönderdiği fonksiyon
+  const sendAnalyticsData = async () => {
+    try {
+      // Ziyaretçinin POST işlemini gerçekleştir
+      await axios.post('http://localhost:3000/analytics');
+
+      // 2 saniyede bir bu fonksiyonu çağır
+      setTimeout(sendAnalyticsData, 2000);
+    } catch (error) {
+      console.error('POST isteği hatası:', error);
+    }
+  };
+
+  // İlk çalıştırmayı yap
+  sendAnalyticsData();
+
   server.all('*', (req, res) => {
     return handle(req, res);
   });
 
-  // Sunucuyu dinleme
   const PORT = process.env.PORT || 3000;
+
   server.listen(PORT, (err) => {
     if (err) throw err;
     console.log(`> Ready on http://localhost:${PORT}`);
